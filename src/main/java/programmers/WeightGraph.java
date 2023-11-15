28587bb01a93e46a10822e3847134749849a807d
MinLee <minlee@gsitm.com>
2023-11-15 10:17:52 +0900

    추가 수정필요함..2

--- src/main/java/programmers/WeightGraph.java
+++ src/main/java/programmers/WeightGraph.java
@@ -3,22 +3,16 @@ package programmers;
 import lombok.ToString;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class WeightGraph {
 
     static List<List<Edge>> list = new ArrayList<>();
-    static final int INF = 2001;
     static String path = "0";
-    static Map<String,Boolean> overVisited;
-    static boolean[] visited;
+    static Set<String> visited;
     static List<Integer> answerList = new ArrayList<>();
     static int K;
     static int N;
-    static int sum;
 
     public static void main(String[] args){
 
@@ -27,8 +21,7 @@ public class WeightGraph {
         N = 6;
         K = 17;
 
-        visited = new boolean[N];
-        overVisited = new HashMap<>();
+        visited = new HashSet<>();
 
         for(int i=0; i<N; i++){
             list.add(new ArrayList<>());
@@ -46,44 +39,45 @@ public class WeightGraph {
         }
 
         dfs(0, 0);
+
+        answerList.forEach(ans-> System.out.println("answer ==> "+ans));
     }
 
+    static String path2 = "0";
+
     static void dfs(int curNode, int curDist){
-        // 0->1 visited[0] true
-        // 1->2 visited[1] true
-        System.out.println("curNode :: "+curNode);
-        System.out.println("curDist :: "+curDist);
-        //System.out.println("path :: "+path);
-
-        /*if(sum >= K){
-            overVisited.put(path, true);
-            if(sum == K) answerList.add(curNode);
-        }*/
-
-        visited[curNode] = true;
 
         for(int i=0; i<list.get(curNode).size(); i++){
             int nextNode = list.get(curNode).get(i).node;
             int weight = list.get(curNode).get(i).weight;
 
-            if(!visited[nextNode] && curDist < 17){
+            if(curDist+weight >= 17){
+                // 17보다 크거나 같으면 더이상 갈 필요가 없음.
+                // 다음노드는 i+1로 가면 됨.
+                if(curDist+weight == 17){
+                    System.out.println("경로 !!!! :: "+path2);
+                    answerList.add(nextNode);
+                }
+                System.out.println("경로 :: "+path2);
+            }else{
+                path2 += nextNode;
                 dfs(nextNode, curDist+weight);
             }
+            /*if(visited.contains(nextPath)){
+                continue;
+            }*/
+
+            //path = nextPath;
+
+            /*if(curDist+weight < 17){
+                dfs(nextNode, curDist+weight);
+            }else{
+                if(curDist+weight == 17) answerList.add(nextNode);
+                visited.add(path);
+                path = "0";
+                dfs(0,0);
+            }*/
         }
-
-        /*for(int i=0; i<list.get(curNode).size(); i++){
-            // curNode 가 0 인경우 1또는 4로 갈 수 있다.
-            int nextNode = list.get(curNode).get(i).node;
-            int weight = list.get(curNode).get(i).weight;
-            sum += weight;
-            if(!overVisited.containsKey(path)) {
-                path += String.valueOf(nextNode);
-                dfs(nextNode);
-            }
-        }*/
-
-        path = "0";
-        visited[curNode] = false;
     }
 
 }

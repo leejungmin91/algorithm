71d0d3ae8c8b8aaf305714fe36d8def3bb8c96b3
Min <leejungmin03@gmail.com>
2023-11-25 00:12:15 +0900

    [프로그래머스] 모의고사 //42839

---
+++ src/main/java/programmers/P42839.java
@@ -0,0 +1,23 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class P42839 {
+
+    public static void main(String[] args){
+
+        String numbers = "17";
+
+        String[] stra = numbers.split("");
+        int[] arr = new int[numbers.length()];
+
+        for(int i=0; i< stra.length; i++){
+            arr[i] = Integer.parseInt(stra[i]);
+        }
+
+    }
+
+}

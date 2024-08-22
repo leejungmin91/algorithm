cbe154282db9db45450b34de243291f31d8368c1
MinLee <minlee@gsitm.com>
2024-08-22 17:44:41 +0900

    Hackerrank 추가 - SparseArrays

---
+++ src/main/java/hackerrank/SparseArrays.java
@@ -0,0 +1,20 @@
+package hackerrank;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SparseArrays {
+
+    public static void main(String[] args) {
+        List<String> stringList = List.of("aba", "baba", "aba", "xzxb");
+        List<String> queries = List.of("aba", "xzxb", "ab");
+
+        System.out.println(matchingStrings(stringList, queries));
+    }
+
+    public static List<Integer> matchingStrings(List<String> stringList, List<String> queries) {
+        //return stringList.stream().map(str -> (int) queries.stream().filter(q -> q.equals(str)).count()).collect(Collectors.toList());
+        return queries.stream().map(q -> (int) stringList.stream().filter(s -> s.equals(q)).count()).collect(Collectors.toList());
+    }
+
+}
